cb3ded2439f516f70cbce51a75d5ed5687d950a6
alexandr <sashkaageev@gmail.com>
2021-03-28 03:22:00 +0300

    Added a custom middleware

--- Platform/Startup.cs
+++ Platform/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,13 +8,9 @@ namespace Platform
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -26,11 +18,34 @@ namespace Platform
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(async (context, next) =>
+            {
+                await next();
+                await context.Response
+                    .WriteAsync($"\nStatus code: {context.Response.StatusCode}");
+            });
+            
+            app.Use(async (ctx, next) =>
+            {
+                if (ctx.Request.Method == HttpMethods.Get
+                && ctx.Request.Query["custom"] == "true")
+                {
+                    await ctx.Response.WriteAsync("Custom Middleware \n");
+                }
+
+                await next();
+            });
+
+            app.UseMiddleware<QueryStringMiddleware>();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
