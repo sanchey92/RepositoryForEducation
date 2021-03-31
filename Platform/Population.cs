8dca33c1dfbb365a74aa202099f1c2b3b6fe6155
alexandr <sashkaageev@gmail.com>
2021-03-31 03:13:10 +0300

    Learning URL Routing

---
+++ Platform/Population.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Platform
+{
+    public class Population
+    {
+        private RequestDelegate _next;
+
+        public Population(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var parts = context.Request.Path
+                .ToString()
+                .Split("/", StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length == 2 && parts[0] == "population")
+            {
+                var city = parts[1];
+                int? pop = city.ToLower() switch
+                {
+                    "london" => 8_136_000,
+                    "paris" => 2_141_000,
+                    "monaco" => 39_000,
+                    _ => null
+                };
+
+                if (pop.HasValue)
+                {
+                    await context.Response
+                        .WriteAsync($"City: {city}, Population: {pop}");
+                    return;
+                }
+            }
+
+            if (_next != null)
+                await _next(context);
+        }
+    }
+}
--- Platform/Startup.cs
+++ Platform/Startup.cs
@@ -2,35 +2,20 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-
 namespace Platform
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<MessageOptions>(options =>
-            {
-                options.CityName = "New York";
-                options.CountryName = "USA";
-            });
-        }
+        public void ConfigureServices(IServiceCollection services) 
+        { }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-                app.UseDeveloperExceptionPage();
-
-            app.UseMiddleware<LocationMiddleware>();
-            
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
+            app.UseDeveloperExceptionPage();
+            app.UseMiddleware<Population>();
+            app.UseMiddleware<Capital>();
+            app.Use(async (context, next) =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                await context.Response.WriteAsync("Terminal Middleware Reached");
             });
         }
     }
