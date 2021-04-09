17418fddffcb211d2132e094a335c03ab30b2c85
alexandr <sashkaageev@gmail.com>
2021-04-09 16:21:50 +0300

    using logging service and users-secrets

--- Platform/Startup.cs
+++ Platform/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Platform
 {
@@ -20,24 +22,38 @@ namespace Platform
             services.Configure<MessageOptions>(Configuration.GetSection("Location"));
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app,
+            IWebHostEnvironment env, ILogger<Startup> logger)
         {
-            app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseRouting();
             app.UseMiddleware<LocationMiddleware>();
 
             app.Use(async (context, next) =>
             {
                 var defaultDebug = Configuration["Logging:LogLevel:Default"];
+                var environ = Configuration["ASPNETCORE_ENVIRONMENT"];
+                var wsId = Configuration["WebService:Id"];
+                var wsKey = Configuration["WebService:Key"];
+
                 await context.Response
-                    .WriteAsync($"The config setting is: {defaultDebug}");
+                    .WriteAsync($"The config setting is: {defaultDebug}\n" +
+                                $"The env setting is: {environ}\n" +
+                                $"The Secret Id is : {wsId}\n" +
+                                $"The Secret Key is: {wsKey}\n");
             });
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
+                    logger.LogDebug("Response for / started");
                     await context.Response.WriteAsync("Hello World!");
+                    logger.LogDebug("Response for / completed");
                 });
             });
         }
