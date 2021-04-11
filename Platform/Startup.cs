31a87121382710dbfc7474f96c5fe57087e636b5
alexandr <sashkaageev@gmail.com>
2021-04-11 18:36:04 +0300

    Using Cookies

--- Platform/Startup.cs
+++ Platform/Startup.cs
@@ -1,53 +1,21 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Platform
 {
     public class Startup
     {
-        public Startup(IConfiguration configService)
-        {
-            Configuration = configService;
-        }
-        
-        private IConfiguration Configuration { get; set; }
+        public void ConfigureServices(IServiceCollection services) { }
 
-        public void ConfigureServices(IServiceCollection services)
+        public void Configure(IApplicationBuilder app)
         {
-            services.Configure<MessageOptions>(Configuration.GetSection("Location"));
-        }
-
-        public void Configure(IApplicationBuilder app, 
-            IWebHostEnvironment env, ILogger<Startup> logger)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseStaticFiles();
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                FileProvider = new PhysicalFileProvider($"{env.ContentRootPath}/staticfiles"),
-                RequestPath = "/files"
-            });
-            
             app.UseRouting();
-            app.UseMiddleware<LocationMiddleware>();
-            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    logger.LogDebug("Response for / started");
                     await context.Response.WriteAsync("Hello World!");
-                    logger.LogDebug("Response for / completed");
                 });
             });
         }
